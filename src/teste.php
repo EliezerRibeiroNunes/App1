c6b79e640845e1c8cc7787be14ee32c0baf6657f
EliezerRibeiroNunes <eliezerrnunes@gmail.com>
2020-08-14 18:47:55 -0300

    add

---
+++ src/teste.php
@@ -0,0 +1,12 @@
+<?php
+
+$num   = 0;
+$num2  = 0;
+$soma;
+
+$num= readline("insira um numero para somar:\n");
+$num2= readline("insira outro numero:\n");
+
+$soma= $num+$num2;
+
+echo "O resultado da soma foi: $soma";
